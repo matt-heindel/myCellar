6673d9d218bf866ee8efa564d78d3fa370d4db33
Matt Heindel <mheindel94@gmail.com>
2021-08-24 14:00:20 -0700

    Implement `App` state from API request

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import NavBar from './NavBar';
 import WineList from './WineList';
 
-let App = (props) => {
+let App = () => {
+  const [wines, setWines] = useState([]);
+
+  useEffect(() => {
+    axios.get('/wines')
+      .then(res => {
+        setWines(res.data);
+      })
+      .catch(err => console.log(err));
+  }, []);
+
   return (
     <>
       <NavBar />
-      <WineList wines={props.wines}/>
+      <WineList wines={wines}/>
     </>
   )
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
-import wines from './wines.json';
 
-
-ReactDOM.render(<App wines={wines} />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
