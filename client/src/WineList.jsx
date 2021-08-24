1376e86fb88d1be1f0bca53be6ca0ec99f2261c0
Matt Heindel <mheindel94@gmail.com>
2021-08-24 12:53:05 -0700

    Render image of `WineListItem`

--- client/src/WineList.jsx
+++ client/src/WineList.jsx
@@ -5,9 +5,23 @@ let WineList = () => {
     <>
       <h2 id="wine-list">Wine List</h2>
       <ul>
-        <li>Wine 1</li>
-        <li>Wine 2</li>
+        <li key="1">
+          <WineListItem />
+        </li>
       </ul>
+    </>
+  )
+}
+
+let WineListItem = () => {
+  return (
+    <>
+      <img
+        className="img"
+        src="https://res.cloudinary.com/my-cellar/image/upload/v1629831425/IMG_0146_by5zbw.jpg"
+        alt="wine 1"
+      ></img>
+      <p>wine 1</p>
     </>
   )
 }
--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.static(__dirname + '/../client/dist'));
+
 app.get('/', (req, res) => {
   res.send('hello from express');
 });
