46d2aaefb2a8bd0308ff33e01346905c1205b47d
Matt Heindel <mheindel94@gmail.com>
2021-08-24 14:29:05 -0700

    Add sort by drop down menu

--- client/src/WineList.jsx
+++ client/src/WineList.jsx
@@ -1,12 +1,20 @@
 import React, { useState } from 'react';
 
 let WineList = (props) => {
-  const sortOptions = ['vintage'];
+  const sortOptions = ['recent vintage', 'drinkability'];
   const [sortBy, setSortBy] = useState(sortOptions[0]);
 
   return (
     <>
       <h2 id="wine-list">Wine List</h2>
+      <form>
+        <label>Sort By:</label>
+        <select name="sortBy" id="sortBy">
+          {sortOptions.map(option => (
+            <option value={option}>{option}</option>
+          ))}
+        </select>
+      </form>
       <ul className="ul-container">
         {props.wines.map(wine => (
           <li key={wine.id}>
