329af380967a370f29fd62cbee8a4c8b8e561d31
Matt Heindel <mheindel94@gmail.com>
2021-08-25 15:09:42 -0700

    Add professional `NavBar` options

--- client/src/NavBar.jsx
+++ client/src/NavBar.jsx
@@ -4,6 +4,13 @@ import myCellar from './assets/myCellar.svg'
 let NavBar = () => (
   <div className='nav-bar'>
     <img id='nav-logo' src={myCellar} alt='wine logo'></img>
+    <div className='nav-container'>
+      <p className='nav-options'> myWines </p>
+      <p className='nav-options'> Account </p>
+      <p className='nav-options'> Docs </p>
+      <p className='nav-options'> About Us </p>
+      <p className='nav-options'> Contact </p>
+    </div>
   </div>
 );
 
