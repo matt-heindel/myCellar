7eb68f0ee865d0c5c52a038a4152915addb9bdbb
Matt Heindel <mheindel94@gmail.com>
2021-08-26 10:41:12 -0700

    Add morgan logging to server

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const db = require('../database/index');
+const morgan = require('morgan');
 
 const app = express();
 
 app.use(express.static(__dirname + '/../client/dist'));
+app.use(morgan('dev'));
 app.use(express.json());
 
 app.get('/wines', (req, res) => {
